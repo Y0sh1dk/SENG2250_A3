3a471123bc3285374b90c6fc6be79aeb94634b51
Y0sh1dk <yosiahdekoeyer@gmail.com>
2021-10-18 00:26:44 +1100

    Setup socket connection

--- src/Client.java
+++ src/Client.java
@@ -1,5 +1,42 @@
-public class Client {
-    public static void main(String[] args) {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
 
+public class Client {
+    private Socket cSocket;
+    private PrintWriter out;
+    private BufferedReader in;
+
+    public static void main(String[] args) {
+        Client c = new Client();
+        try {
+            c.run(Protocol.getPortNumber());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    private void run(int portNumber) throws IOException {
+        this.setupConnection(portNumber);
+
+        // DO stuff
+
+        this.terminate();
+    }
+
+    private void setupConnection(int portNumber) throws IOException {
+        this.cSocket = new Socket(Protocol.getIPAddress(), portNumber);
+        this.out = new PrintWriter(this.cSocket.getOutputStream(), true);
+        this.in = new BufferedReader(new InputStreamReader(this.cSocket.getInputStream()));
+    }
+
+    private void terminate() throws IOException {
+        this.in.close();
+        this.out.close();
+        this.cSocket.close();
     }
 }
--- src/Server.java
+++ src/Server.java
@@ -1,5 +1,42 @@
-public class Server {
-    public static void main(String[] args) {
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
 
+public class Server {
+    private ServerSocket sSocket;
+    private Socket clientSocket;
+    private PrintWriter out;
+    private BufferedReader in;
+
+    public static void main(String[] args) {
+        Server s = new Server();
+        try {
+            s.run(Protocol.getPortNumber());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    private void run(int portNumber) throws IOException {
+        this.setupConnection(portNumber);
+
+        // Do stuff
+
+        this.terminate();
+    }
+
+    private void setupConnection(int portNumber) throws IOException {
+        this.sSocket = new ServerSocket(portNumber);
+        this.clientSocket = this.sSocket.accept();
+        this.out = new PrintWriter(this.clientSocket.getOutputStream(), true);
+        this.in = new BufferedReader(new InputStreamReader(this.clientSocket.getInputStream()));
+    }
+
+    private void terminate() throws IOException {
+        this.in.close();
+        this.out.close();
+        this.clientSocket.close();
+        this.sSocket.close();
     }
 }
